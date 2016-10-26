01839ef57c8598869447900e93331091f11625f0
Joris Ooms <info@jorisooms.be>
2016-10-26 23:00:06 +0200

    Initial commit

---
+++ Main.elm
@@ -0,0 +1,186 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Html.App as App
+import String
+import Dict exposing (Dict)
+import Char
+
+
+type alias Model =
+    { inputValue : String
+    , code : String
+    }
+
+
+initialModel : Model
+initialModel =
+    Model "" ""
+
+
+charMap : Dict Char String
+charMap =
+    [97..123]
+        |> List.map (\x -> ( Char.fromCode x, toString (x - 96) ))
+        |> Dict.fromList
+
+
+type alias Icon =
+    { url : String
+    , text : String
+    }
+
+
+icons : List Icon
+icons =
+    [ { url = "http://www.kevindecock.be/apps/supercrypt/img/icons/dots.png"
+      , text = "1. Enter a fun message to your kids below."
+      }
+    , { url = "http://www.kevindecock.be/apps/supercrypt/img/icons/hash_bourgondy.png"
+      , text = "2. Magic! It gets encoded with a super secret key."
+      }
+    , { url = "http://www.kevindecock.be/apps/supercrypt/img/icons/questionmark_orange.png"
+      , text = "3. Let your kids guess the code - how fast can they figure out the message?"
+      }
+    ]
+
+
+type Msg
+    = Input String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input val ->
+            { model
+                | inputValue = val
+                , code = getCodeFromValue val
+            }
+
+
+getCodeFromValue : String -> String
+getCodeFromValue text =
+    text
+        |> String.toLower
+        |> String.toList
+        |> List.map mapCharToCode
+        |> List.intersperse "-"
+        |> String.join ""
+
+
+mapCharToCode : Char -> String
+mapCharToCode str =
+    case (Dict.get str charMap) of
+        Just value ->
+            value
+
+        Nothing ->
+            String.fromChar str
+
+
+headerView : Html Msg
+headerView =
+    div [ class "row" ]
+        [ nav
+            [ class "top-bar"
+            , attribute "data-topbar" "true"
+            , attribute "role" "navigation"
+            ]
+            [ ul [ class "title-area" ]
+                [ li [ class "name" ]
+                    [ h1 []
+                        [ img
+                            [ src "http://www.kevindecock.be/apps/supercrypt/img/logo_text_white.png"
+                            , alt "supercrypt!"
+                            , id "logoimg"
+                            ]
+                            []
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+
+iconView : Icon -> Html Msg
+iconView icon =
+    div
+        [ class "small-12 medium-4 large-4 columns text-center"
+        , attribute "style" "height: 320px;"
+        ]
+        [ img
+            [ class "smallicon"
+            , src icon.url
+            ]
+            []
+        , p []
+            [ text icon.text ]
+        ]
+
+
+iconsView : Html Msg
+iconsView =
+    div
+        [ class "row" ]
+        (List.map iconView icons)
+
+
+encoderView : Model -> Html Msg
+encoderView model =
+    div [ class "row" ]
+        [ div
+            [ class "small-12 medium-5 large-5 columns my-panel with-arrow-left"
+            ]
+            [ h2 [ class "text-center", attribute "style" "color:white" ]
+                [ text "Just start typing:" ]
+            , input
+                [ onInput Input
+                , placeholder "What would you like to encode?"
+                , type' "text"
+                ]
+                []
+            ]
+        , div
+            [ class "small-12 medium-6 large-6 columns my-panel dotted"
+            , id "resultID"
+            , attribute "style" "height: inherit;"
+            ]
+            [ h2 [ class "text-center" ]
+                [ text "Here's the result:" ]
+            , p
+                []
+                [ text model.code ]
+            ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ headerView
+        , div
+            [ class "page page-main" ]
+            [ div [ class "view-container" ]
+                [ div [ class "row" ]
+                    [ div [ class "small-12 medium-12 medium-centered columns" ]
+                        [ h1 [ class "text-center" ]
+                            [ text "Let's play SuperCrypt#!" ]
+                        ]
+                    ]
+                , iconsView
+                , encoderView model
+                ]
+            ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
